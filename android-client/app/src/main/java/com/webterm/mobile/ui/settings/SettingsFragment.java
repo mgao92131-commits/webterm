6ed61291e19f80426bb7834a09f7ef66ffa38252
gao <3237324890@qq.com>
2026-07-04 10:34:29 +0800

    feat: 新建 SettingsFragment 骨架

---
+++ android-client/app/src/main/java/com/webterm/mobile/ui/settings/SettingsFragment.java
@@ -0,0 +1,23 @@
+package com.webterm.mobile.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class SettingsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return new android.widget.FrameLayout(requireContext());
+    }
+}
